4d50306018ee994427b629b99a3d41b0675221fe
christopher <christopherjoshuasoares@hotmail.com>
2025-02-19 14:50:11 -0300

    completado

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -596,6 +596,26 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports,__globalThis) {
+AOS.init();
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs / 1000);
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado';
+    }
+}, 1000);
 
 },{}]},["k51Nd","3cYfC"], "3cYfC", "parcelRequire94c2")
 
